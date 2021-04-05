3304ac93d22aec452be0fd5749b92565c559a6bc
Roshan Kr. Mahato <76640700+Ros-an@users.noreply.github.com>
2021-04-05 09:20:57 +0530

    added content

---
+++ src/Modal.js
@@ -0,0 +1,12 @@
+import React, { useEffect } from "react";
+
+export const Modal = () => {
+  //   useEffect(() => {
+  //     setTimeout(() => xyz, 2000);
+  //   });
+  return (
+    <div className="modal">
+      <p>this is modal text</p>
+    </div>
+  );
+};
