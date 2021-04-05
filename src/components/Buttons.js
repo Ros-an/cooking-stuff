a47fb5c479a6a82045556015bb7f337bfff842e1
Roshan Kr. Mahato <76640700+Ros-an@users.noreply.github.com>
2021-04-05 09:16:37 +0530

    initial commit

---
+++ src/components/Buttons.js
@@ -0,0 +1,9 @@
+import "./button.css";
+
+export const AllProducts = () => {
+  return (
+    <div className="all-products-btn">
+      <button className="all-products pointer-cursor">ALL PRODUCTS</button>
+    </div>
+  );
+};
---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./footer.css";
+
+export const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="footer--heading">Made in India</div>
+      <div className="image">
+        <img
+          src="https://deploy-preview-1--roshanuidesign.netlify.app/images/Flag_of_India.svg"
+          alt="Indian-flag"
+        />
+      </div>
+    </div>
+  );
+};
