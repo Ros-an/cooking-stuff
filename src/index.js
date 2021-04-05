81145f6360e62d77e6c73eb7b8dc3f2925e5b9ca
Roshan Kr. Mahato <76640700+Ros-an@users.noreply.github.com>
2021-04-05 09:20:36 +0530

    added GlobalProvider

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { GlobalProvider } from "./globalContext";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <GlobalProvider>
+      <App />
+    </GlobalProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
