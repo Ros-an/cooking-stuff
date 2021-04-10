6d98255656df7ebd86cc0aeb6f3b90ee78045bc7
Roshan Kr. Mahato <76640700+Ros-an@users.noreply.github.com>
2021-04-10 09:52:32 +0530

    feat: remove, add to cart

--- src/global-reducer.js
+++ src/global-reducer.js
@@ -1,5 +1,37 @@
 import { useReducer } from "react";
 
 export const reducer = (state, action) => {
-  return state;
+  const { type, payLoad } = action;
+  const { cart, wishList, products } = state;
+  switch (type) {
+    case "ADD_TO_CART":
+      const newProducts = products.map((item) => {
+        return item.id === payLoad.id ? { ...item, isAddedToCart: true } : item;
+      });
+      return {
+        ...state,
+        products: newProducts,
+        cart: cart.concat(payLoad),
+        showModal: true,
+        modalContent: "added to cart",
+      };
+    case "REMOVE_FROM_CART":
+      const newCart = cart.filter((item) => item.id !== payLoad.id);
+      const newProducts1 = products.map((item) => {
+        return item.id === payLoad.id
+          ? { ...item, isAddedToCart: false }
+          : item;
+      });
+      return {
+        ...state,
+        products: newProducts1,
+        cart: newCart,
+        showModal: true,
+        modalContent: "removed from cart",
+      };
+    case "CLOSE_MODAL":
+      return { ...state, showModal: false };
+    default:
+      return state;
+  }
 };
