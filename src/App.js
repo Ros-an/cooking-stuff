8d0449645dcd859eeb742d9957e7f1b6d2b3f32a
Roshan Kr. Mahato <76640700+Ros-an@users.noreply.github.com>
2021-04-10 09:47:01 +0530

    added router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import React, { useState } from "react";
-import Navbar from "./components/Navbar";
-import { ProductSection } from "./components/ProductionSection";
-import { Modal } from "./Modal";
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import { Home } from "./components/Home";
+import { Cart } from "./components/Cart";
 import { Footer } from "./components/Footer";
-import Slideshow from "./components/SlideShow";
+import Navbar from "./components/Navbar";
+import { Modal } from "./Modal";
 import { useGlobal } from "./globalContext";
 
 function App() {
@@ -12,8 +13,14 @@ function App() {
     <>
       {state.showModal && <Modal />}
       <Navbar />
-      <Slideshow />
-      <ProductSection />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/cart">
+          <Cart />
+        </Route>
+      </Switch>
       <Footer />
     </>
   );
