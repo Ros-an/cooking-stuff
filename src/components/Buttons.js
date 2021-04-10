02c5466f478d21c2b0febcc3103e043c20db7ffd
Roshan Kr. Mahato <76640700+Ros-an@users.noreply.github.com>
2021-04-10 09:47:33 +0530

    added addToCartButton

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,9 +1,27 @@
-import "./button.css";
+import "./buttons.css";
+import { useGlobal } from "../globalContext";
 
 export const AllProducts = () => {
   return (
     <div className="all-products-btn">
       <button className="all-products pointer-cursor">ALL PRODUCTS</button>
     </div>
+  );
+};
+
+export const AddToCartButton = ({ item }) => {
+  const { dispatch } = useGlobal();
+  return (
+    <>
+      <button
+        class="btn-product cart-btn"
+        onClick={() => dispatch({ type: "ADD_TO_CART", payLoad: item })}
+      >
+        <span>
+          <i class="fas fa-cart-plus"></i>
+        </span>
+        <span> {item.isAddedToCart ? "ADDED" : "ADD TO CART"}</span>
+      </button>
+    </>
   );
 };
