f94f5a43670fe4f4c0b31742d7bd0df417bb24cc
Roshan Kr. Mahato <76640700+Ros-an@users.noreply.github.com>
2021-04-10 09:53:27 +0530

    added Router

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import { BrowserRouter as Router } from "react-router-dom";
 import { GlobalProvider } from "./globalContext";
 
 ReactDOM.render(
   <React.StrictMode>
-    <GlobalProvider>
-      <App />
-    </GlobalProvider>
+    <Router>
+      <GlobalProvider>
+        <App />
+      </GlobalProvider>
+    </Router>
   </React.StrictMode>,
   document.getElementById("root")
 );
