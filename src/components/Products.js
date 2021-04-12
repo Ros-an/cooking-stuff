20bbed748623bccd5c86911b7453b2fbb66ab834
Roshan Kr. Mahato <76640700+Ros-an@users.noreply.github.com>
2021-04-12 08:15:55 +0530

    refactor: image of card

--- src/components/Products.js
+++ src/components/Products.js
@@ -3,24 +3,26 @@ import { AddToCartButton } from "./Buttons";
 export const Products = () => {
   const { state } = useGlobal();
   return (
-    <div className="product-section__subsection product-part">
-      {state.products.map((item) => {
-        return (
-          <div class="card">
-            <div class="card-image">
-              <img className="product-img" src={item.image} />
-              <button class="pointer-cursor">❤</button>
+    <>
+      <div className="product-section__subsection product-part">
+        {state.products.map((item) => {
+          return (
+            <div class="card">
+              <div class="card-image">
+                <img className="product-img" src={item.image} />
+                <button class="pointer-cursor">❤</button>
+              </div>
+              <div class="card-content">
+                <p>Rating</p>
+                <h4>Product name</h4>
+                <p>₹ {item.price}</p>
+                <p>little</p>
+                <AddToCartButton item={item} />
+              </div>
             </div>
-            <div class="card-content">
-              <p>Rating</p>
-              <h4>Product name</h4>
-              <p>price</p>
-              <p>little</p>
-              <AddToCartButton item={item} />
-            </div>
-          </div>
-        );
-      })}
-    </div>
+          );
+        })}
+      </div>
+    </>
   );
 };
