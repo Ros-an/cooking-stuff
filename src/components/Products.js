6df3c04d81bc0631870a8378dacccf709d08df4d
Roshan Kr. Mahato <76640700+Ros-an@users.noreply.github.com>
2021-04-13 21:58:42 +0530

    feat: toggling effect of wishlist icon

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,16 +1,41 @@
 import { useGlobal } from "../globalContext";
 import { AddToCartButton } from "./Buttons";
 export const Products = () => {
-  const { state } = useGlobal();
+  const { state, dispatch } = useGlobal();
+
+  const customStyle = {
+    color: "red",
+  };
+  function wishItemToggle(id) {
+    if (state.wishList.some((e) => e.id === id)) {
+      return customStyle;
+    }
+    return null;
+  }
   return (
     <>
       <div className="product-section__subsection product-part">
         {state.products.map((item) => {
+          const { inStock } = item;
           return (
-            <div class="card">
+            <div class="card" style={{ boxShadow: !inStock && "none" }}>
               <div class="card-image">
-                <img className="product-img" src={item.image} />
-                <button class="pointer-cursor">❤</button>
+                <img src={item.image} />
+                <button
+                  style={wishItemToggle(item.id)}
+                  class="pointer-cursor"
+                  onClick={() => {
+                    dispatch({ type: "ADD_TO_WISHL", payLoad: item });
+                  }}
+                >
+                  ❤
+                </button>
+              </div>
+              <div
+                class="out-of-stock"
+                style={{ visibility: inStock ? "hidden" : "visible" }}
+              >
+                <div class="out-of-stock--tag">OUT OF STOCK</div>
               </div>
               <div class="card-content">
                 <p>Rating</p>
