ebd9add6afaa7c9ac1a1552980835bf370363611
Roshan Kr. Mahato <76640700+Ros-an@users.noreply.github.com>
2021-04-13 21:55:18 +0530

    feat: rendering different component based on cart length

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,10 +1,41 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import "./cart.css";
 import { useGlobal } from "../globalContext";
 import { CartItem } from "./CartItem";
 
 export const Cart = () => {
   const { state, dispatch } = useGlobal();
+  if (state.cart.length === 0) {
+    return (
+      <div
+        style={{
+          height: "70vh",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <h2>Cart is empty</h2>
+
+        <NavLink
+          to="/"
+          activeStyle={{
+            textDecoration: "none",
+            color: "white",
+          }}
+        >
+          <button
+            style={{ padding: "0.25rem 1rem", fontSize: "1.15rem" }}
+            className="btn-danger"
+          >
+            Fill it
+          </button>
+        </NavLink>
+      </div>
+    );
+  }
   return (
     <>
       <div className="cart-block">
