274f8a8989229859e90b6acb4f92e92bec6b3986
Roshan Kr. Mahato <76640700+Ros-an@users.noreply.github.com>
2021-04-13 21:53:27 +0530

    add: wishlist component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
-import { Route, Switch } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import { Home } from "./components/Home";
 import { Cart } from "./components/Cart";
 import { Footer } from "./components/Footer";
 import Navbar from "./components/Navbar";
 import { Modal } from "./Modal";
 import { useGlobal } from "./globalContext";
+import WishList from "./components/WishList";
 
 function App() {
   const { state } = useGlobal();
@@ -13,14 +14,13 @@ function App() {
     <>
       {state.showModal && <Modal />}
       <Navbar />
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route path="/cart">
-          <Cart />
-        </Route>
-      </Switch>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+
+        <Route path="/cart" element={<Cart />} />
+
+        <Route path="/wishlist" element={<WishList />} />
+      </Routes>
       <Footer />
     </>
   );
