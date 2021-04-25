a8e3285659391bfbe07af1deeedfd65d2b356ce5
Roshan Kr. Mahato <76640700+Ros-an@users.noreply.github.com>
2021-04-25 17:21:11 +0530

    setup: route for productsection

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router-dom";
 import { Home } from "./components/Home";
 import { Cart } from "./components/Cart";
 import { Footer } from "./components/Footer";
+import { ProductSection } from "./components/ProductionSection";
 import Navbar from "./components/Navbar";
 import { Modal } from "./Modal";
 import { useGlobal } from "./globalContext";
@@ -20,6 +21,7 @@ function App() {
         <Route path="/cart" element={<Cart />} />
 
         <Route path="/wishlist" element={<WishList />} />
+        <Route path="/products" element={<ProductSection />} />
       </Routes>
       <Footer />
     </>
