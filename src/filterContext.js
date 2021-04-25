faac31bb251682d4f0d22600ffbfdcc738263cf4
Roshan Kr. Mahato <76640700+Ros-an@users.noreply.github.com>
2021-04-25 21:47:17 +0530

    setup: filterContext

---
+++ src/filterContext.js
@@ -0,0 +1,33 @@
+import React, { createContext, useReducer, useContext, useEffect } from "react";
+import { useGlobal } from "./globalContext";
+import reducer from "./filter-reducer";
+
+export const FilterContext = createContext();
+
+const defaultState = {
+  filtered_products: [],
+  all_products: [],
+  sort: "low-to-high",
+};
+export const FilterProvider = ({ children }) => {
+  const { state } = useGlobal();
+  const [filtered_state, dispatch] = useReducer(reducer, defaultState);
+
+  useEffect(() => {
+    dispatch({ type: "LOADING_PRODUCTS", payLoad: state.products });
+  }, [state.products]);
+  const updateSort = (e) => {
+    // console.log(e.target);
+    const { name, value } = e.target;
+    dispatch({ type: "UPDATE_SORT", payLoad: value });
+  };
+  return (
+    <FilterContext.Provider value={{ ...filtered_state, dispatch, updateSort }}>
+      {children}
+    </FilterContext.Provider>
+  );
+};
+
+export const useFilterContext = () => {
+  return useContext(FilterContext);
+};
