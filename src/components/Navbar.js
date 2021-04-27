6082488a62ea72c6794dbd62b60453379d372b1b
Roshan Kr. Mahato <76640700+Ros-an@users.noreply.github.com>
2021-04-27 19:46:27 +0530

    refactor: structured corresponding to flex

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,60 +6,49 @@ const NavBar = () => {
   const { state } = useGlobal();
   const { cart, wishList } = state;
   return (
-    <div className="container-grid navbar">
+    <div className="container-flex navbar">
       <div className="logo pointer-cursor nav-item--effect">
         {" "}
         <span className="logo-small">
           <i class="fas fa-utensils"></i>
         </span>{" "}
-        <span className="logo-large">Cooking-Stuff</span>
+        <span className="logo-large">
+          <NavLink to="/">Cooking-Stuff</NavLink>
+        </span>
       </div>
-      <div className="search"></div>
-      <div className="home pointer-cursor nav-item--effect">
-        <NavLink
-          to="/"
-          activeStyle={{
-            textDecoration: "none",
-          }}
-        >
-          Home
-        </NavLink>
-      </div>
-      <div className="about pointer-cursor nav-item--effect">
-        <div class="badge-icon">
-          <NavLink
-            to="/cart"
-            activeStyle={{
-              textDecoration: "none",
-            }}
-          >
-            <i class="fas fa-shopping-bag icon"></i>
-            <span
-              class="badge-count--status"
-              style={{ visibility: cart.length ? "visible" : "hidden" }}
-            >
-              {cart.length}
-            </span>
-          </NavLink>
+      <div className="nav-item">
+        <div className="home pointer-cursor nav-item--effect">
+          <NavLink to="/">Home</NavLink>
         </div>
-        <div class="badge-icon">
-          <NavLink
-            to="/wishlist"
-            activeStyle={{
-              textDecoration: "none",
-            }}
-          >
-            <i class="fas fa-heart icon"></i>
-            <span
-              class="badge-count--status"
-              style={{ visibility: wishList.length ? "visible" : "hidden" }}
-            >
-              {wishList.length}
-            </span>
-          </NavLink>
+        <div className="home pointer-cursor nav-item--effect">
+          <NavLink to="/products">Products</NavLink>
         </div>
+        <div className="about pointer-cursor nav-item--effect">
+          <div class="badge-icon">
+            <NavLink to="/cart">
+              <i class="fas fa-shopping-bag icon"></i>
+              <span
+                class="badge-count--status"
+                style={{ visibility: cart.length ? "visible" : "hidden" }}
+              >
+                {cart.length}
+              </span>
+            </NavLink>
+          </div>
+          <div class="badge-icon">
+            <NavLink to="/wishlist">
+              <i class="fas fa-heart icon"></i>
+              <span
+                class="badge-count--status"
+                style={{ visibility: wishList.length ? "visible" : "hidden" }}
+              >
+                {wishList.length}
+              </span>
+            </NavLink>
+          </div>
+        </div>
+        <div className="login pointer-cursor nav-item--effect">User</div>
       </div>
-      <div className="login pointer-cursor nav-item--effect">User</div>
     </div>
   );
 };
