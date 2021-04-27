0ded8a9b0703adaa94fb6c93e1c4193884f61186
Roshan Kr. Mahato <76640700+Ros-an@users.noreply.github.com>
2021-04-27 19:37:17 +0530

    initial commit

---
+++ src/components/ProductPage.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./css/productpage.css";
+import { useGlobal } from "../globalContext";
+import { useParams } from "react-router-dom";
+
+function ProductPage() {
+  const { state } = useGlobal();
+  const { id } = useParams();
+  const product = state.products.find((product) => product.id === parseInt(id));
+  return (
+    <div className="single-product">
+      <div className="section-image">
+        <img src={product.image} alt={product.name} />
+      </div>
+      <div className="section-detail">
+        <h1>this is product name</h1>
+        <h3>this is product rating</h3>
+        <h3>{product.price}</h3>
+      </div>
+    </div>
+  );
+}
+
+export default ProductPage;
