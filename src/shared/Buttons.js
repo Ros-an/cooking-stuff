663478079b44dbdd9b8d28d49e28d023318a880e
Roshan Kr. Mahato <76640700+Ros-an@users.noreply.github.com>
2021-05-08 07:39:15 +0530

    removed: AddToCartButton component

--- src/shared/Buttons.js
+++ src/shared/Buttons.js
@@ -16,22 +16,3 @@ export const AllProducts = () => {
     </div>
   );
 };
-
-export const AddToCartButton = ({ item }) => {
-  const { dispatch } = useGlobal();
-  return (
-    <>
-      <button
-        disabled={item.isAddedToCart}
-        style={{ margin: "0" }}
-        class="btn-product cart-btn"
-        onClick={() => dispatch({ type: "ADD_TO_CART", payLoad: item })}
-      >
-        <span>
-          <i class="fas fa-cart-plus"></i>
-        </span>
-        <span> {item.isAddedToCart ? "ADDED" : "ADD TO CART"}</span>
-      </button>
-    </>
-  );
-};
