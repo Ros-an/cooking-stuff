eefcc2444171dbbd26bcf7c4e7cfdcf8d0e9846e
Roshan Kr. Mahato <76640700+Ros-an@users.noreply.github.com>
2021-05-08 07:35:49 +0530

    added: brand component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Slideshow from "../components/SlideShow";
 import { AllProducts } from "../shared/Buttons";
+import Brand from "../components/Brand";
 
 export const Home = () => {
   return (
     <>
       <Slideshow />
       <AllProducts />
+      <Brand />
     </>
   );
 };
