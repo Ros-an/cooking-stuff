bc144a1e98f332f50ec8519dc5429534d59d89b1
Roshan Kr. Mahato <76640700+Ros-an@users.noreply.github.com>
2021-05-10 08:22:23 +0530

    changed: slide image

--- src/components/SlideShow.js
+++ src/components/SlideShow.js
@@ -4,7 +4,7 @@ import { Slide } from "react-slideshow-image";
 
 const slideImages = [
   "https://coverfiles.alphacoders.com/108/108897.jpg",
-  "https://images.unsplash.com/photo-1546702005-7f8e5aeab4a6?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1500&q=80",
+  "https://images.unsplash.com/photo-1564385222835-80264869c85c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1489&q=80",
   "https://i.pinimg.com/originals/9a/04/6d/9a046d8113aa415ea7bacf11ea7c824f.jpg",
 ];
 
