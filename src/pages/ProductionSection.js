58fe43eb3e01ab15ce96da1c23a81d20df2d05b4
Roshan Kr. Mahato <76640700+Ros-an@users.noreply.github.com>
2021-05-19 08:17:14 +0530

    feat: loader

--- src/pages/ProductionSection.js
+++ src/pages/ProductionSection.js
@@ -1,7 +1,16 @@
-import "./productsection.css";
+import { useFilterContext } from "../context/filterContext";
 import { Products } from "../components/Products";
 import Filter from "../components/filter";
+import Loading from "./Loader";
+
+import "./productsection.css";
+
 export const ProductSection = () => {
+  const { filtered_products } = useFilterContext();
+
+  if (!filtered_products.length) {
+    return <Loading />;
+  }
   return (
     <div className="product-section">
       <Filter />
