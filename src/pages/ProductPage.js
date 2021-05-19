e4bb0f2d0dcb220f9a69b282489bdc49d512d200
Roshan Kr. Mahato <76640700+Ros-an@users.noreply.github.com>
2021-05-19 08:18:44 +0530

    refactor: in terms fetching data from server

--- src/pages/ProductPage.js
+++ src/pages/ProductPage.js
@@ -1,13 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import "./productpage.css";
 import { useGlobal } from "../context/globalContext";
 import { useParams, NavLink } from "react-router-dom";
 import Rating from "../components/Rating";
+import Loading from "./Loader";
 
 function ProductPage() {
-  const { state, dispatch } = useGlobal();
+  const [product, setProduct] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [showLess, setShowLess] = useState(true);
+  const { dispatch } = useGlobal();
   const { id } = useParams();
-  const product = state.products.find((product) => product.id === parseInt(id));
+  console.log("roshan", id);
+  useEffect(() => {
+    async function getData() {
+      try {
+        setLoading(true);
+        const response = await axios.get(
+          `https://cooking-stuff-backend.rosan.repl.co/api/products/${id}`
+        );
+        console.log(response);
+        setProduct(response.data.product);
+      } catch (err) {
+        console.log(err);
+      }
+      setLoading(false);
+    }
+    getData();
+  }, [id]);
+  if (loading) {
+    return <Loading />;
+  }
   return (
     <>
       <NavLink to="/products">
@@ -15,7 +39,7 @@ function ProductPage() {
       </NavLink>
       <div className="single-product">
         <div className="section-image">
-          <img src={product.image} alt={product.name} />
+          <img src={product.imageUrl} alt={product.title} />
           <div
             className="out-of-stock"
             style={{ visibility: product.inStock ? "hidden" : "visible" }}
@@ -47,10 +71,21 @@ function ProductPage() {
           </div>
         </div>
         <div className="section-detail">
-          <h1>this is product name</h1>
+          <h1>{product.title}</h1>
           <Rating rating={product.rating} />
           <h3>Rs. {product.price}</h3>
-          <p>Description detail</p>
+          <h2>Description</h2>
+          <p>
+            {showLess
+              ? `${product.description.substring(0, 200)}...`
+              : product.description}
+            <span
+              onClick={() => setShowLess(!showLess)}
+              className="pointer-cursor showMore"
+            >
+              {showLess ? " show more" : " show less"}
+            </span>
+          </p>
         </div>
       </div>
     </>
