f03a25b75831e3176561d3c50305c9228c81e9e9
Roshan Kr. Mahato <76640700+Ros-an@users.noreply.github.com>
2021-07-10 14:29:55 +0530

    feat: navigation control depending on authority

--- src/shared/Navbar.js
+++ src/shared/Navbar.js
@@ -1,71 +1,88 @@
 import React from "react";
 import "./Navbar.css";
 import { NavLink, useNavigate } from "react-router-dom";
-import { useGlobal } from "../context/globalContext";
 import { auth } from "../firebase/firebase";
 import { useAuthContext } from "../context/authContext";
+import { useCartContext } from "../context/cartContext";
+
 function NavBar() {
-  const { state } = useGlobal();
+  const { cart, setCart } = useCartContext();
   let navigate = useNavigate();
   const { userData, setUserData } = useAuthContext();
-  const { cart, wishList } = state;
   const logout = () => {
     auth.signOut();
     setUserData(null);
-    navigate(`/authenticate`);
+    setCart(null);
   };
   return (
     <div className="container-flex navbar">
       <div className="logo pointer-cursor nav-item--effect">
-        <NavLink to="/">
+        <div onClick={() => navigate(`/`)}>
           {" "}
           <span className="logo-small">
             <i className="fas fa-utensils"></i>
           </span>{" "}
           <span className="logo-large">Cooking-Stuff</span>
-        </NavLink>
+        </div>
       </div>
       <div className="nav-item">
         <div className="home pointer-cursor nav-item--effect">
-          <NavLink to="/">Home</NavLink>
+          <NavLink to="/" end>
+            Home
+          </NavLink>
         </div>
         <div className="home pointer-cursor nav-item--effect">
           <NavLink to="/products">Products</NavLink>
         </div>
-        <div className="about pointer-cursor nav-item--effect">
-          <div className="badge-icon">
-            <NavLink to="/cart">
-              <i className="fas fa-shopping-bag icon"></i>
+
+        <div className="pointer-cursor nav-item--effect">
+          <NavLink to="/cart">
+            <div className="badge-icon">
+              <i className="fas fa-shopping-bag nav-icon"></i>
               <span
                 className="badge-count--status"
-                style={{ visibility: cart.length ? "visible" : "hidden" }}
+                style={{ visibility: cart?.length ? "visible" : "hidden" }}
               >
-                {cart.length}
+                {cart?.length}
               </span>
-            </NavLink>
-          </div>
-          <div className="badge-icon">
-            <NavLink to="/wishlist">
-              <i className="fas fa-heart icon"></i>
+            </div>
+          </NavLink>
+        </div>
+        <div className="pointer-cursor nav-item--effect">
+          <NavLink to="/wishlist">
+            <div className="badge-icon">
+              <i className="fas fa-heart nav-icon"></i>
               <span
                 className="badge-count--status"
-                style={{ visibility: wishList.length ? "visible" : "hidden" }}
+                style={{
+                  visibility: userData?.wishlist.length ? "visible" : "hidden",
+                }}
               >
-                {wishList.length}
+                {userData?.wishlist.length}
               </span>
-            </NavLink>
-          </div>
+            </div>
+          </NavLink>
         </div>
         {!userData ? (
           <NavLink to="/authenticate">
             <div className="login pointer-cursor nav-item--effect">
-              <span className="logo-large">SignIn</span>
+              <span>
+                <i className="fas fa-sign-in-alt nav-icon"></i>
+              </span>
             </div>
           </NavLink>
         ) : (
-          <div className="pointer-cursor nav-item--effect" onClick={logout}>
-            SignOut
-          </div>
+          <NavLink
+            to="/authenticate"
+            className="pointer-cursor nav-item--effect"
+            onClick={logout}
+          >
+            <div className="login pointer-cursor nav-item--effect">
+              <span>
+                <i className="fas fa-sign-out-alt nav-icon"></i>
+              </span>
+            </div>
+          </NavLink>
         )}
       </div>
     </div>
