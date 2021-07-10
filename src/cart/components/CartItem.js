216340f10dbad8cd977f911b072556445bd14cf9
Roshan Kr. Mahato <76640700+Ros-an@users.noreply.github.com>
2021-07-10 14:47:51 +0530

    feat: add to wishlist, remove from cart, alterquantity

--- src/cart/components/CartItem.js
+++ src/cart/components/CartItem.js
@@ -1,63 +1,100 @@
-import React from "react";
+import React, { useState } from "react";
+import { addToWishList, removeFromCart } from "../../products/index";
+import { useAuthContext } from "../../context/authContext";
+import { useCartContext } from "../../context/cartContext";
+import { present } from "../../products/pages/SingleProduct";
+import { alterQuantity } from "../index";
+import { MiniLoader } from "../../shared/Loader";
+
 import "../pages/cart.css";
 
-export const CartItem = ({ state, dispatch }) => {
+export const CartItem = ({ product, quantity }) => {
+  const { setCart } = useCartContext();
+  const { userData, setUserData } = useAuthContext();
+  const [loader, setLoader] = useState(false);
+  const [loaderOne, setLoaderOne] = useState(false);
+  const [loaderTwo, setLoaderTwo] = useState(false);
+  const [loaderThree, setLoaderThree] = useState(false);
+  function addItemToWishlist() {
+    addToWishList(product._id, userData, setLoader, setUserData);
+  }
+  const removeItem = () => {
+    removeFromCart(product._id, userData, setLoaderOne, setCart);
+  };
+  function changeQuantity(productId, type) {
+    if (type === "dec") {
+      alterQuantity(userData._id, productId, type, setCart, setLoaderTwo);
+    } else {
+      alterQuantity(userData._id, productId, type, setCart, setLoaderThree);
+    }
+  }
   return (
-    <>
-      {state.cart.map((item) => {
-        const { _id, imageUrl, quantity } = item;
-        return (
-          <div className="horizontal-card-row" key={_id}>
-            <div className="horizontal-card__image">
-              <img src={imageUrl} alt="img_" />
-            </div>
-            <div className="product-detail">
-              <div className="product-quantity">
-                <p className="product-name">Product name</p>
-                <p className="product-price">Price</p>
-                <button
-                  className="minus"
-                  onClick={() =>
-                    dispatch({ type: "COUNT_DOWN", payLoad: item })
-                  }
-                >
-                  <i className="fas fa-angle-double-left"></i>
-                </button>
-                <button className="quantity">{quantity}</button>
-                <button
-                  className="plus"
-                  onClick={() =>
-                    dispatch({ type: "COUNT_UP", payLoad: item.id })
-                  }
-                >
-                  <i className="fas fa-angle-double-right"></i>
-                </button>
-              </div>
-              <div className="horizontal-card__button">
-                <button
-                  onClick={() =>
-                    dispatch({
-                      type: "ADD_TO_WISHLIST",
-                      payLoad: item,
-                    })
-                  }
-                  className="remove-btn wish-btn"
-                >
-                  WISHLIST
-                </button>
-                <button
-                  className="remove-btn"
-                  onClick={() =>
-                    dispatch({ type: "REMOVE_FROM_CART", payLoad: item.id })
-                  }
-                >
-                  <i className="fas fa-trash-alt"></i>
-                </button>
-              </div>
-            </div>
-          </div>
-        );
-      })}
-    </>
+    <div className="horizontal-card-row">
+      <div className="horizontal-card__image">
+        <img src={product.imageUrl} alt={product.title} />
+      </div>
+      <div className="product-detail">
+        <div className="product-quantity">
+          <h4 className="product-name">{product.title}</h4>
+          <p className="product-price">Rs.{product.price}</p>
+          <button
+            className="minus pointer-cursor"
+            style={{ cursor: quantity === 1 && "no-drop" }}
+            disabled={quantity === 1 && true}
+            onClick={() => changeQuantity(product._id, "dec")}
+          >
+            {loaderTwo ? (
+              <MiniLoader
+                size={{ height: "1em", width: "1em" }}
+                spinner={true}
+              />
+            ) : (
+              <i className="fas fa-angle-double-left"></i>
+            )}
+          </button>
+          <button className="quantity">{quantity}</button>
+          <button
+            className="plus pointer-cursor"
+            onClick={() => changeQuantity(product._id, "inc")}
+          >
+            {loaderThree ? (
+              <MiniLoader
+                spinner={true}
+                size={{ height: "1em", width: "1em" }}
+              />
+            ) : (
+              <i className="fas fa-angle-double-right"></i>
+            )}
+          </button>
+        </div>
+        <div className="horizontal-card__button">
+          <button
+            onClick={() => addItemToWishlist()}
+            className="remove-btn wish-btn pointer-cursor"
+          >
+            {loader ? (
+              <MiniLoader size={{ height: "1em", width: "1em" }} />
+            ) : present(userData.wishlist, product._id) ? (
+              "ADDED"
+            ) : (
+              "WISHLIST"
+            )}
+          </button>
+          <button
+            className="remove-btn pointer-cursor"
+            onClick={() => removeItem()}
+          >
+            {loaderOne ? (
+              <MiniLoader
+                spinner={true}
+                size={{ height: "1em", width: "1em" }}
+              />
+            ) : (
+              <i className="fas fa-trash-alt"></i>
+            )}
+          </button>
+        </div>
+      </div>
+    </div>
   );
 };
