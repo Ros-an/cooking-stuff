b5e75f00cf8f6b2953632a7bea8616ebbd6436e6
Roshan Kr. Mahato <76640700+Ros-an@users.noreply.github.com>
2021-07-10 14:23:33 +0530

    refacotor: moved card as separate component

--- src/wishlist/pages/WishList.js
+++ src/wishlist/pages/WishList.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import { useGlobal } from "../../context/globalContext";
-import Rating from "../../products/components/Rating";
+import { useAuthContext } from "../../context/authContext";
+import WishListItem from "../components/WishListItem";
 import "./Wishlist.css";
 function WishList() {
-  const { state, dispatch } = useGlobal();
-  if (state.wishList.length === 0) {
+  const { userData } = useAuthContext();
+  if (userData?.wishlist.length === 0) {
     return (
       <div
         style={{
@@ -38,51 +38,10 @@ function WishList() {
   return (
     <div className="wishlist">
       <p className="section-heading">WishList</p>
-      <p className="cart-count">No. of item: {state.wishList.length}</p>
+      <p className="cart-count">No. of item: {userData?.wishlist.length}</p>
       <div className="wishlist__card-section">
-        {state.wishList.map((item) => {
-          const { inStock } = item;
-          return (
-            <div className="card" key={item.id}>
-              <div className="card-image">
-                <img src={item.image} alt="img_" />
-                <span
-                  className="card-badge--remove pointer-cursor"
-                  onClick={() =>
-                    dispatch({ type: "REMOVE_FROM_WISHLIST", payLoad: item.id })
-                  }
-                >
-                  <i className="far fa-times-circle"></i>
-                </span>
-              </div>
-              <div
-                className="out-of-stock"
-                style={{ visibility: inStock ? "hidden" : "visible" }}
-              >
-                <div className="out-of-stock--tag">OUT OF STOCK</div>
-              </div>
-              <div className="card-content">
-                <h4>Product name</h4>
-                <Rating rating={item.rating} />
-                <p>{item.price}</p>
-                <p>little</p>
-                <div className="wishlist-btn">
-                  <button
-                    onClick={() =>
-                      dispatch({
-                        type: "ADD_TO_CART_FROM_WISHLIST",
-                        payLoad: item,
-                      })
-                    }
-                    className="add-cart"
-                  >
-                    {" "}
-                    ADD TO CART
-                  </button>
-                </div>
-              </div>
-            </div>
-          );
+        {userData?.wishlist.map((item) => {
+          return <WishListItem key={item._id} item={item} />;
         })}
       </div>
     </div>
